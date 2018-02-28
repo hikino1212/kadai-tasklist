4a26c22167668c2a9d5ee97deed1ca1b1e18e343
hajime <ec2-user@ip-172-31-39-97.us-east-2.compute.internal>
2018-02-28 15:22:37 +0000

    add column 2

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
   validates :content, presence: true, length: { maximum: 11 }
-  validates :Status, presence: true, length: { maximum: 11 }
+  validates :status, presence: true, length: { maximum: 11 }
 end
