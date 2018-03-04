55608c6177ec648df927b8da2f83b587d89273a5
hajime <ec2-user@ip-172-31-39-97.us-east-2.compute.internal>
2018-03-04 08:04:59 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  get 'toppages/index'
 
-  resources :tasks
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  #  root to: 'tasks#index'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
